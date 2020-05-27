219b7b4bb21ee1a6c2e71dde4486f4861826a493
Kinkandju <ferretsphilosophy@gmail.com>
2020-05-27 17:46:55 +0300

    Исправление ошибок, удаление лишних комментариев, отдельная группировка переменных и функций.

--- js/base.js
+++ js/base.js
@@ -1,19 +1,18 @@
 'use strict';
 
-// Данные о фаерболе
 window.fireballSize = 22;
+window.wizardWidth = 70;
+window.wizardSpeed = 3;
+
+
 window.getFireballSpeed = function (movingLeft) {
   return movingLeft ? 5 : 2;
-// var result = условие ? значение1 : значение2;
-// Сначала вычисляется условие: если оно истинно, тогда возвращается значение1, в противном случае – значение2.
 };
 
-// Данные о волшебнике
-window.wizardWidth = 70;
-window.getWizardHeight = function () {
-  return 1.337 * window.wizardWidth;
+window.getWizardHeight = function (width) {
+  return 1.337 * width;
 };
-window.wizardSpeed = 3;
+
 window.getWizardX = function (width) {
   return (width - window.wizardWidth) / 2;
 };
