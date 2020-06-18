851cece2ce2c4d7e496db460fa1b66aed71861fa
Kinkandju <ferretsphilosophy@gmail.com>
2020-06-18 21:29:06 +0300

    Устранение замечаний.

--- js/dialog.js
+++ js/dialog.js
@@ -1,8 +1,51 @@
 'use strict';
 
 (function () {
-
   var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = document.querySelector('.setup-close');
+  var wizardName = document.querySelector('.setup-user-name');
+
+  var onEscPress = function (evt) {
+    if (evt.key === 'Escape' && evt.target !== wizardName) {
+      evt.preventDefault();
+      closePopup();
+    }
+  };
+
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+
+    document.addEventListener('keydown', onEscPress);
+  };
+
+  var closePopup = function () {
+    setup.classList.add('hidden');
+
+    document.removeEventListener('keydown', onEscPress);
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+    setup.style = '';
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      openPopup();
+    }
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      closePopup();
+    }
+  });
+
   var dialogHandle = setup.querySelector('.upload');
 
   dialogHandle.addEventListener('mousedown', function (evt) {
@@ -53,5 +96,4 @@
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   });
-
 })();
--- js/utils.js
+++ js/utils.js
@@ -1,27 +1,26 @@
 'use strict';
 
 (function () {
-
   window.utils = {
     getRandomInteger: function (min, max) {
       return Math.floor(min + Math.random() * (max + 1 - min));
     },
 
     getRandomElement: function (elements) {
-      var randomItemIndex = window.utils.getRandomInteger(0, elements.length - 1);
+      var randomItemIndex = this.getRandomInteger(0, elements.length - 1);
       return elements[randomItemIndex];
     },
 
     getRandomName: function () {
-      return window.utils.getRandomElement(window.setup.WIZARD_NAMES) + ' ' + window.utils.getRandomElement(window.setup.WIZARD_SURNAMES);
+      return this.getRandomElement(window.setup.WIZARD_NAMES) + ' ' + this.getRandomElement(window.setup.WIZARD_SURNAMES);
     },
 
     getRandomCoats: function () {
-      return window.utils.getRandomElement(window.setup.WIZARD_COATS);
+      return this.getRandomElement(window.setup.WIZARD_COATS);
     },
 
     getRandomEyes: function () {
-      return window.utils.getRandomElement(window.setup.WIZARD_EYES);
+      return this.getRandomElement(window.setup.WIZARD_EYES);
     }
   };
 })();
